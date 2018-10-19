ea47792a9214e0c26131ff8cfeb2d5303d015acf
Pieter-Jan Vanhaverbeke <pieter-jan.vanhaverbeke@student.kuleuven.be>
2018-10-19 12:20:21 +0200

    testupdate

--- src/GameMain.java
+++ src/GameMain.java
@@ -2,6 +2,7 @@ public class GameMain {
     public static void main(String[] args) {
         Memoryspel memoryspel = new Memoryspel(2, 1);
         memoryspel.start();
+        System.out.println();
 
 
     }
