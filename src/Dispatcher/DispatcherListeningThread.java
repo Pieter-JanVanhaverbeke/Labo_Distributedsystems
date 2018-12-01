ffe17623bd02eec3a75325c80a691f1debdca834
Pieter-Jan Vanhaverbeke <pieter-jan.vanhaverbeke@student.kuleuven.be>
2018-12-01 14:38:01 +0100

    begin dispatcher

---
+++ src/Dispatcher/DispatcherListeningThread.java
@@ -0,0 +1,20 @@
+package Dispatcher;
+
+public class DispatcherListeningThread {
+
+
+
+
+    public  DispatcherListeningThread(String client){
+
+    }
+
+    public void run(){
+        while(true){
+
+            //makeserverconnection
+            //Handel connection af
+        }
+    }
+
+}
--- src/Dispatcher/DispatcherMain.java
+++ src/Dispatcher/DispatcherMain.java
@@ -1,4 +1,18 @@
 package Dispatcher;
 
 public class DispatcherMain {
+
+
+    public void startDispatcher(){
+        Dispatcher dispatcher = new Dispatcher();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        DispatcherMain main = new DispatcherMain();
+        main.startDispatcher();
+    }
+
 }
